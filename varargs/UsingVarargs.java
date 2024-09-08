9bcb87dcd7647c2bd550cc6629f40835759adff6
Sopheary Rin <104388521+sophearyrin-dev@users.noreply.github.com>
2024-09-08 04:26:44 +0000

    done variable argument

--- varargs/UsingVarargs.java
+++ varargs/UsingVarargs.java
@@ -1,5 +1,26 @@
 public class UsingVarargs{
-    public static void main(String args[]){
-        System.out.println("Hello");
+    public static int sumNumbers(int... numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
     }
+
+    public static int testLength(int a, int... arrs){
+        return arrs.length;
+    }
+
+    public static void main(String[] args) {
+        // Passing individual numbers
+        System.out.println("Sum of 1, 2: " + sumNumbers(1, 2));                // Output: 3
+        
+        // Passing an array of numbers
+        int[] numberArray = {1, 2, 3, 4, 5};
+        System.out.println("Sum of array: " + sumNumbers(numberArray));        // Output: 15
+
+        // Passing args to see the length
+        System.out.println(testLength(1,1,2,3));    
+    }
+    
 }
