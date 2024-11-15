13f664e6960d0df942ca8b9055eb675cbc2f39f7
Sopheary Rin <104388521+sophearyrin-dev@users.noreply.github.com>
2024-11-15 19:47:22 +0000

    indexOf

--- hashset/Main.java
+++ hashset/Main.java
@@ -1,13 +1,11 @@
 package hashset;
 
-import java.util.HashSet;
-
 public class Main {
     public static void main(String[] args) {
-        var set = new HashSet<>();
+       //indexOf - substring
+       String a = "Hi, Hello World";
+       String b = "Hello";
 
-        set.add(1);
-
-        System.out.println(set.add(2));
+       System.out.println(a.indexOf(b));
     }
 }
