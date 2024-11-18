66e2978aa590469ee9d2bc164a932942d44a99e1
Sopheary Rin <104388521+sophearyrin-dev@users.noreply.github.com>
2024-11-18 17:08:46 +0000

    carry

--- hashset/Main.java
+++ hashset/Main.java
@@ -7,5 +7,9 @@ public class Main {
 
        //substring(startIndex, endIndex+1);
        System.out.println(a.substring(6,11));
+
+       //carry
+       int b = 8, c = 7;
+       System.out.println("Carry= " + (b+c)/10);
     }
 }
